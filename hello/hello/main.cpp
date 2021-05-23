c10f92b0765ed9e99d7b41d1f05f812143aef921
Nolan Kitchin <nolankitch@gmail.com>
2021-05-22 19:40:04 -0500

    Initial commit

---
+++ hello/hello/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  hello
+//
+//  Created by Nolan Kitchin on 5/22/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
